69b764696cd77485ca5773e73a6af1de404c139d
oliverbeumkes-nhs <oliver.beumkes2@nhs.net>
2025-09-25 11:38:16 +0100

    formatted variables for mhs

--- stacks/mhs/terraform/variables.tf
+++ stacks/mhs/terraform/variables.tf
@@ -52,7 +52,7 @@ variable "mhs_spine_request_max_size" {
 
 variable "build_id" {
   description = "ID used to identify the current build such as a commit sha."
-  default = "2.0.1"
+  default     = "2.0.1"
 }
 
 variable "deregistration_delay" {
