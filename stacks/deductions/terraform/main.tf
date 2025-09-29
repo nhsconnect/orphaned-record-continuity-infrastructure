2ff8051cb543883f11eec11299662f1e6a091dd6
oliverbeumkes-nhs <oliver.beumkes2@nhs.net>
2025-09-29 15:22:28 +0100

    Re-enabled sts region

--- stacks/deductions/terraform/main.tf
+++ stacks/deductions/terraform/main.tf
@@ -5,6 +5,7 @@ provider "aws" {
 provider "aws" {
   alias  = "ci"
   region = var.region
+  sts_region = "us-east-1"
   assume_role {
     role_arn     = "arn:aws:iam::${var.common_account_id}:role/GitHub-Actions-ReadOnly"
     session_name = "common-${var.environment}-cross-account"
