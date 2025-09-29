e09063145229fee892f0ad952cae38c0b0fb9fa2
oliverbeumkes-nhs <oliver.beumkes2@nhs.net>
2025-09-29 17:24:55 +0100

    Handle mhs infra

--- stacks/mhs/terraform/backend.tf
+++ stacks/mhs/terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket  = "prm-deductions-terraform-state"
-    key     = "mhs-infra/terraform.tfstate"
+    key     = "mhs/terraform.tfstate"
     region  = "eu-west-2"
     encrypt = true
   }
