86727de700e67fca67c447eb110879761e54bcd2
oliverbeumkes-nhs <oliver.beumkes2@nhs.net>
2025-11-11 11:34:09 +0000

    Updated terraform provider version

--- stacks/dev-cross-account-changes/terraform/iam.tf
+++ stacks/dev-cross-account-changes/terraform/iam.tf
@@ -10,8 +10,21 @@ data "aws_iam_policy_document" "ecr_pull_through" {
     sid     = "PullCreateImport"
     effect  = "Allow"
     actions = [
+      "ecr:GetDownloadUrlForLayer",
       "ecr:BatchGetImage",
-      "ecr:GetDownloadUrlForLayer"
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:PutImage",
+      "ecr:InitiateLayerUpload",
+      "ecr:UploadLayerPart",
+      "ecr:CreateRepository",
+      "ecr:CompleteLayerUpload",
+      "ecr:DescribeRepositories",
+      "ecr:GetRepositoryPolicy",
+      "ecr:ListImages",
+      "ecr:DeleteRepository",
+      "ecr:BatchDeleteImage",
+      "ecr:SetRepositoryPolicy",
+      "ecr:DeleteRepositoryPolicy",
     ]
     resources = ["*"]
   }
