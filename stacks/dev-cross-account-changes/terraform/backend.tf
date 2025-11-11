bd83d6d589c2d9902158a89c592f9b0c653a5502
oliverbeumkes-nhs <oliver.beumkes2@nhs.net>
2025-11-11 11:16:43 +0000

    Updated terraform provider version

--- stacks/dev-cross-account-changes/terraform/backend.tf
+++ stacks/dev-cross-account-changes/terraform/backend.tf
@@ -1,11 +1,11 @@
 terraform {
+  required_version = ">= 1.5.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.60.0"
+      version = ">= 6.0" # or pin to a recent 6.x
     }
   }
-
   backend "s3" {
     encrypt = true
   }
