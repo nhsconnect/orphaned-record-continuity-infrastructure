342e8c22381dbd56de17e05a68aea223f5c19ed8
oliverbeumkes-nhs <oliver.beumkes2@nhs.net>
2025-11-11 12:31:24 +0000

    Chaged vars to bash syntax

--- stacks/dev-cross-account-changes/terraform/ecr.tf
+++ stacks/dev-cross-account-changes/terraform/ecr.tf
@@ -65,7 +65,6 @@ resource "aws_ecr_repository_creation_template" "mhs_out" {
   EOT
   description = "Defaults for repos auto-created under mhs-out/* by ECR PTC"
   repository_policy = data.aws_iam_policy_document.ecr_pull_through.json
-
 }
 
 resource "aws_secretsmanager_secret" "dockerhub_ecr_ptc" {
@@ -80,4 +79,36 @@ resource "aws_secretsmanager_secret_version" "dockerhub_ecr_ptc" {
     username    = var.dockerhub_username
     accessToken = var.dockerhub_access_token
   })
+}
+
+data "aws_iam_policy_document" "ecr_pull_through" {
+  statement {
+    sid       = "AuthToken"
+    effect    = "Allow"
+    actions   = ["ecr:GetAuthorizationToken"]
+    resources = ["*"]
+  }
+
+  statement {
+    sid     = "PullCreateImport"
+    effect  = "Allow"
+    actions = [
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:PutImage",
+      "ecr:InitiateLayerUpload",
+      "ecr:UploadLayerPart",
+      "ecr:CreateRepository",
+      "ecr:CompleteLayerUpload",
+      "ecr:DescribeRepositories",
+      "ecr:GetRepositoryPolicy",
+      "ecr:ListImages",
+      "ecr:DeleteRepository",
+      "ecr:BatchDeleteImage",
+      "ecr:SetRepositoryPolicy",
+      "ecr:DeleteRepositoryPolicy",
+    ]
+    resources = ["*"]
+  }
 }
