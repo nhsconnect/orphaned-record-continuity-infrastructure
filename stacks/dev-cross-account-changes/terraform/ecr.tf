4d003720dbe7fb8510bfcc3f50f3693922bfb55c
oliverbeumkes-nhs <oliver.beumkes2@nhs.net>
2025-11-11 12:28:33 +0000

    Chaged vars to bash syntax

--- stacks/dev-cross-account-changes/terraform/ecr.tf
+++ stacks/dev-cross-account-changes/terraform/ecr.tf
@@ -1,31 +1,3 @@
-locals {
-  ecr_lifecycle_policy_text = jsonencode({
-    rules = [
-      {
-        rulePriority = 10
-        description  = "Expire untagged images after var.untagged_expire_days days"
-        selection = {
-          tagStatus   = "untagged"
-          countType   = "sinceImagePushed"
-          countUnit   = "days"
-          countNumber = var.untagged_expire_days
-        }
-        action = { type = "expire" }
-      },
-      {
-        rulePriority = 20
-        description  = "Keep only the last var.max_images_per_repo tagged images"
-        selection = {
-          tagStatus   = "any"
-          countType   = "imageCountMoreThan"
-          countNumber = var.max_images_per_repo
-        }
-        action = { type = "expire" }
-      }
-    ]
-  })
-}
-
 data "aws_caller_identity" "this" {}
 
 resource "aws_secretsmanager_secret_policy" "dockerhub_ecr_ptc" {
@@ -72,7 +44,25 @@ resource "aws_ecr_repository_creation_template" "mhs_out" {
     encryption_type = "AES256"
   }
 
-  lifecycle_policy = local.ecr_lifecycle_policy_text
+ lifecycle_policy = <<EOT
+  {
+    "rules": [
+      {
+        "rulePriority": 1,
+        "description": "Expire images older than 14 days",
+        "selection": {
+          "tagStatus": "untagged",
+          "countType": "sinceImagePushed",
+          "countUnit": "days",
+          "countNumber": 14
+        },
+        "action": {
+          "type": "expire"
+        }
+      }
+    ]
+  }
+  EOT
   description = "Defaults for repos auto-created under mhs-out/* by ECR PTC"
   repository_policy = data.aws_iam_policy_document.ecr_pull_through.json
 
