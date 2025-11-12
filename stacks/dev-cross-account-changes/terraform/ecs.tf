780199ce4e92ffccdde1ac67bd58c9ec11ad759b
oliverbeumkes-nhs <oliver.beumkes2@nhs.net>
2025-11-12 13:16:27 +0000

    Added ecs task that required image not in ecr ptc repo to test initiation

--- stacks/dev-cross-account-changes/terraform/ecs.tf
+++ stacks/dev-cross-account-changes/terraform/ecs.tf
@@ -1,3 +1,52 @@
 resource "aws_ecs_cluster" "my_cluster" {
   name = "temp-cluster" # Name your cluster here
 }
+
+# main.tf
+resource "aws_ecs_task_definition" "temp_task" {
+  family                   = "temp-first-task" # Name your task
+  container_definitions    = <<DEFINITION
+  [
+    {
+      "name": "temp-first-task",
+      "image": "docker-hub/nhsdev/nia-mhs-inbound",
+      "essential": true,
+      "portMappings": [
+        {
+          "containerPort": 5000,
+          "hostPort": 5000
+        }
+      ],
+      "memory": 512,
+      "cpu": 256
+    }
+  ]
+  DEFINITION
+  requires_compatibilities = ["FARGATE"] # use Fargate as the launch type
+  network_mode             = "awsvpc"    # add the AWS VPN network mode as this is required for Fargate
+  memory                   = 512         # Specify the memory the container requires
+  cpu                      = 256         # Specify the CPU the container requires
+  execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole.arn}"
+}
+
+# main.tf
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+  name               = "ecsTaskExecutionRole"
+  assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
+  role       = "${aws_iam_role.ecsTaskExecutionRole.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
