820f28c46e17a320fc47264bb37bb0169f77a60e
chrisbloe <kris.bloe2@nhs.net>
2025-11-26 16:47:12 +0000

    Removing resources not available in aws provider v3

--- stacks/deductions-cross-account/terraform/ecr.tf
+++ stacks/deductions-cross-account/terraform/ecr.tf
@@ -70,15 +70,6 @@ resource "aws_ecr_repository" "mesh-forwarder" {
   }
 }
 
-
-resource "aws_ecr_repository" "mesh-inbox-s3-forwarder" {
-  name                 = "deductions/mesh-inbox-s3-forwarder"
-  image_tag_mutability = var.immutable_ecr_repositories ? "IMMUTABLE" : "MUTABLE"
-  tags = {
-    CreatedBy = var.repo_name
-  }
-}
-
 resource "aws_ecr_repository" "nems-event-processor" {
   name                 = "deductions/nems-event-processor"
   image_tag_mutability = var.immutable_ecr_repositories ? "IMMUTABLE" : "MUTABLE"
@@ -123,14 +114,16 @@ data "aws_caller_identity" "this" {}
 
 resource "aws_secretsmanager_secret_policy" "dockerhub_ecr_ptc" {
   secret_arn = aws_secretsmanager_secret.dockerhub_ecr_ptc.arn
-  policy     = jsonencode({
+  policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
       {
-        Sid      = "AllowEcrToReadForPullThroughCache",
-        Effect   = "Allow",
-        Principal = { Service = "ecr.amazonaws.com" },
-        Action   = [
+        Sid    = "AllowEcrToReadForPullThroughCache",
+        Effect = "Allow",
+        Principal = {
+          Service = "ecr.amazonaws.com"
+        },
+        Action = [
           "secretsmanager:DescribeSecret",
           "secretsmanager:GetSecretValue"
         ],
@@ -148,85 +141,16 @@ resource "aws_secretsmanager_secret_policy" "dockerhub_ecr_ptc" {
   })
 }
 
-resource "aws_ecr_pull_through_cache_rule" "docker_hub" {
-  ecr_repository_prefix = "docker-hub"
-  upstream_registry_url = "registry-1.docker.io"
-  credential_arn        = aws_secretsmanager_secret.dockerhub_ecr_ptc.arn
-  depends_on = [aws_secretsmanager_secret_policy.dockerhub_ecr_ptc]
-}
-
-resource "aws_ecr_repository_creation_template" "docker_hub" {
-  prefix      = "docker-hub"
-  applied_for = ["PULL_THROUGH_CACHE"]
-
-  image_tag_mutability = "IMMUTABLE"
-
-  encryption_configuration {
-    encryption_type = "AES256"
-  }
-
- lifecycle_policy = <<EOT
-  {
-    "rules": [
-      {
-        "rulePriority": 1,
-        "description": "Expire images older than 14 days",
-        "selection": {
-          "tagStatus": "untagged",
-          "countType": "sinceImagePushed",
-          "countUnit": "days",
-          "countNumber": 14
-        },
-        "action": {
-          "type": "expire"
-        }
-      }
-    ]
-  }
-  EOT
-  description = "Defaults for repos auto-created under mhs-out/* by ECR PTC"
-  repository_policy = data.aws_iam_policy_document.ecr_pull_through.json
-}
-
 resource "aws_secretsmanager_secret" "dockerhub_ecr_ptc" {
   name        = "ecr-pullthroughcache/dockerhub"
   description = "Docker Hub creds for ECR pull-through cache"
 }
 
 resource "aws_secretsmanager_secret_version" "dockerhub_ecr_ptc" {
-  secret_id     = aws_secretsmanager_secret.dockerhub_ecr_ptc.id
-  # Use Docker Hub username + PAT (read-only)
+  secret_id = aws_secretsmanager_secret.dockerhub_ecr_ptc.id
+
   secret_string = jsonencode({
     username    = var.dockerhub_username
     accessToken = var.dockerhub_access_token
   })
-}
-
-data "aws_iam_policy_document" "ecr_pull_through" {
-  statement {
-    sid    = "new policy"
-    effect = "Allow"
-
-    principals {
-      type        = "AWS"
-      identifiers = [data.aws_caller_identity.this.account_id]
-    }
-
-    actions = [
-      "ecr:GetDownloadUrlForLayer",
-      "ecr:BatchGetImage",
-      "ecr:BatchCheckLayerAvailability",
-      "ecr:PutImage",
-      "ecr:InitiateLayerUpload",
-      "ecr:UploadLayerPart",
-      "ecr:CompleteLayerUpload",
-      "ecr:DescribeRepositories",
-      "ecr:GetRepositoryPolicy",
-      "ecr:ListImages",
-      "ecr:DeleteRepository",
-      "ecr:BatchDeleteImage",
-      "ecr:SetRepositoryPolicy",
-      "ecr:DeleteRepositoryPolicy",
-    ]
-  }
 }
--- stacks/deductions-cross-account/terraform/variables.tf
+++ stacks/deductions-cross-account/terraform/variables.tf
@@ -24,16 +24,8 @@ variable "dockerhub_username" {
   type = string
   sensitive = true
 }
+
 variable "dockerhub_access_token" {
   type = string
   sensitive = true
-}
-# lifecycle tuning
-variable "max_images_per_repo"   { 
-  type = number
-  default = 30 
- }
-variable "untagged_expire_days"  { 
-  type = number
-  default = 7 
 }
