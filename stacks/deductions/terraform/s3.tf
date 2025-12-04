8b3c21aad6863652b8625bcf82f09ac0732ede98
oliverbeumkes-nhs <oliver.beumkes2@nhs.net>
2025-12-04 16:11:59 +0000

    [PRM-481] REverting removal of permission for 652711504416 s3:PutObject and allow_load_balancers_to_publish_to_access_logs_s3_bucket

--- stacks/deductions/terraform/s3.tf
+++ stacks/deductions/terraform/s3.tf
@@ -191,7 +191,7 @@ resource "aws_s3_bucket_versioning" "alb_access_logs" {
 
 resource "aws_s3_bucket_policy" "alb_access_logs_policy" {
   bucket = aws_s3_bucket.alb_access_logs.id
-  policy = data.aws_iam_policy_document.deny_load_balancers_to_publish_to_access_logs_s3_bucket.json
+  policy = data.aws_iam_policy_document.allow_load_balancers_to_publish_to_access_logs_s3_bucket.json
 }
 
 resource "aws_s3_bucket_public_access_block" "alb_access_logs" {
@@ -203,18 +203,15 @@ resource "aws_s3_bucket_public_access_block" "alb_access_logs" {
   restrict_public_buckets = true
 }
 
-data "aws_iam_policy_document" "deny_load_balancers_to_publish_to_access_logs_s3_bucket" {
+
+data "aws_iam_policy_document" "allow_load_balancers_to_publish_to_access_logs_s3_bucket" {
   statement {
-    effect = "Deny"
     principals {
       type        = "AWS"
-      identifiers = ["*"]
+      identifiers = ["arn:aws:iam::652711504416:root"]
     }
-    actions = ["s3:PutObject"]
-    resources = [
-      aws_s3_bucket.alb_access_logs.arn,
-      "${aws_s3_bucket.alb_access_logs.arn}/*"
-    ]
+    actions   = ["s3:PutObject"]
+    resources = ["${aws_s3_bucket.alb_access_logs.arn}/*"]
   }
 }
 
