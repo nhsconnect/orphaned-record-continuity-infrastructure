71485ae005cbe3bc8575d408d70d253bc1bd1c75
oliverbeumkes-nhs <oliver.beumkes2@nhs.net>
2025-12-05 16:35:37 +0000

    Update s3.tf

--- stacks/deductions/terraform/s3.tf
+++ stacks/deductions/terraform/s3.tf
@@ -204,7 +204,7 @@ resource "aws_s3_bucket_public_access_block" "alb_access_logs" {
 }
 
 data "aws_iam_policy_document" "allow_load_balancers_to_publish_to_access_logs_s3_bucket" {
-   statement {
+  statement {
     principals {
       type        = "AWS"
       identifiers = ["arn:aws:iam::652711504416:root"]
