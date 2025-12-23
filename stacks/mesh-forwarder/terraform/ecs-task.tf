d6c609c3399ab218d7e5e7f77ca72ac9b587f8a9
oliverbeumkes-nhs <oliver.beumkes2@nhs.net>
2025-12-23 10:05:18 +0000

    [PRM-495] Revert read-only filesystem fir mesh-forwarder

--- stacks/mesh-forwarder/terraform/ecs-task.tf
+++ stacks/mesh-forwarder/terraform/ecs-task.tf
@@ -26,7 +26,6 @@ resource "aws_ecs_task_definition" "forwarder" {
       image                  = "${data.aws_ecr_repository.mesh_s3_forwarder.repository_url}:${var.task_image_tag}"
       environment            = local.environment_variables
       essential              = true
-      readonlyRootFilesystem = true
       logConfiguration = {
         logDriver = "awslogs"
         options = {
